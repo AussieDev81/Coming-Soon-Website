c31151a48d2040461d3c62eb9c85bbd972e3d883
AussieDev81 <aussiedev81@gmail.com>
2023-03-04 15:44:02 +1000

    Added Google reCAPTCHA

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,15 @@
-
 document.getElementById("subscribe").addEventListener("submit", (event) => {
 	event.preventDefault();
 	const subscriber = {
 		created_at: new Date(),
 		email: event.target["email"].value,
 	};
-	addSubscriber(subscriber);
-    event.target.reset();
+
+	if (grecaptcha.getResponse() != 0) {
+		addSubscriber(subscriber);
+		event.target.reset();
+        grecaptcha.reset();
+	}
 });
 
 const addSubscriber = async (subscriber) => {
@@ -14,7 +17,7 @@ const addSubscriber = async (subscriber) => {
 	const URL = `https://bmtshsmfotakquvzsnsk.supabase.co/rest/v1/${TABLE}`;
 	const API_KEY =
 		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtdHNoc21mb3Rha3F1dnpzbnNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc4MjI5OTQsImV4cCI6MTk5MzM5ODk5NH0.sv0uAXcxMAS5JPt_n3Ti4VBBw4-xL4VmhxKGDjNa9O0";
-    const HELP_EMAIL = "help@crowdlinx.com";
+	const HELP_EMAIL = "help@crowdlinx.com";
 	const HEADERS = new Headers({
 		"Content-Type": "application/json",
 		"Access-Control-Allow-Origin": "*",
@@ -28,11 +31,15 @@ const addSubscriber = async (subscriber) => {
 		body: JSON.stringify(subscriber),
 	})
 		.then((response) => {
-			if (response.status == 201) alert(`Thanks!\n${subscriber.email} has been added and we'll keep you updated 😀`); //201 - Created
-			else alert(`Hmm well this is embarrassing 🤔\nPlease wait a minute and then try again, and if the problem persists please email us at ${HELP_EMAIL}`);
+			if (response.status == 201)
+				alert(`Thanks!\n${subscriber.email} has been added and we'll keep you updated 😀`); //201 - Created
+			else
+				alert(
+					`Hmm well this is embarrassing 🤔\nPlease wait a minute and then try again, and if the problem persists please email us at ${HELP_EMAIL}`
+				);
 		})
 		.catch((error) => {
-            alert("Whoops... Something went wrong");
-            console.log(error)
-        });
+			alert("Whoops... Something went wrong");
+			console.log(error);
+		});
 };
